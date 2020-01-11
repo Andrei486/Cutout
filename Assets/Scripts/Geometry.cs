2bdae92053aaf0094d5a066c6b013b5123e87b97
Andrei486 <55627866+Andrei486@users.noreply.github.com>
2020-01-10 22:07:37 -0500

    Added cutouts, lines gone
    Lines can no longer be drawn: any object that does not loop back is destroyed.
    Objects successfully created are now filled and leave white cutouts in their place.
    Cutouts have no effect yet.
    Also changed the way drawing distance scales with screen size.

---
+++ Assets/Scripts/Geometry.cs
@@ -0,0 +1,196 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+namespace Geometry {
+	
+	public class Triangulator
+	{
+		//Triangulator class taken from the Unity Community Wiki.
+		//All credit goes to runevision for this class.
+		private List<Vector2> m_points = new List<Vector2>();
+	 
+		public Triangulator (Vector2[] points) {
+			m_points = new List<Vector2>(points);
+		}
+	 
+		public int[] Triangulate() {
+			List<int> indices = new List<int>();
+	 
+			int n = m_points.Count;
+			if (n < 3)
+				return indices.ToArray();
+	 
+			int[] V = new int[n];
+			if (Area() > 0) {
+				for (int v = 0; v < n; v++)
+					V[v] = v;
+			}
+			else {
+				for (int v = 0; v < n; v++)
+					V[v] = (n - 1) - v;
+			}
+	 
+			int nv = n;
+			int count = 2 * nv;
+			for (int v = nv - 1; nv > 2; ) {
+				if ((count--) <= 0)
+					return indices.ToArray();
+	 
+				int u = v;
+				if (nv <= u)
+					u = 0;
+				v = u + 1;
+				if (nv <= v)
+					v = 0;
+				int w = v + 1;
+				if (nv <= w)
+					w = 0;
+	 
+				if (Snip(u, v, w, nv, V)) {
+					int a, b, c, s, t;
+					a = V[u];
+					b = V[v];
+					c = V[w];
+					indices.Add(a);
+					indices.Add(b);
+					indices.Add(c);
+					for (s = v, t = v + 1; t < nv; s++, t++)
+						V[s] = V[t];
+					nv--;
+					count = 2 * nv;
+				}
+			}
+	 
+			indices.Reverse();
+			return indices.ToArray();
+		}
+	 
+		private float Area () {
+			int n = m_points.Count;
+			float A = 0.0f;
+			for (int p = n - 1, q = 0; q < n; p = q++) {
+				Vector2 pval = m_points[p];
+				Vector2 qval = m_points[q];
+				A += pval.x * qval.y - qval.x * pval.y;
+			}
+			return (A * 0.5f);
+		}
+	 
+		private bool Snip (int u, int v, int w, int n, int[] V) {
+			int p;
+			Vector2 A = m_points[V[u]];
+			Vector2 B = m_points[V[v]];
+			Vector2 C = m_points[V[w]];
+			if (Mathf.Epsilon > (((B.x - A.x) * (C.y - A.y)) - ((B.y - A.y) * (C.x - A.x))))
+				return false;
+			for (p = 0; p < n; p++) {
+				if ((p == u) || (p == v) || (p == w))
+					continue;
+				Vector2 P = m_points[V[p]];
+				if (InsideTriangle(A, B, C, P))
+					return false;
+			}
+			return true;
+		}
+	 
+		private bool InsideTriangle (Vector2 A, Vector2 B, Vector2 C, Vector2 P) {
+			float ax, ay, bx, by, cx, cy, apx, apy, bpx, bpy, cpx, cpy;
+			float cCROSSap, bCROSScp, aCROSSbp;
+	 
+			ax = C.x - B.x; ay = C.y - B.y;
+			bx = A.x - C.x; by = A.y - C.y;
+			cx = B.x - A.x; cy = B.y - A.y;
+			apx = P.x - A.x; apy = P.y - A.y;
+			bpx = P.x - B.x; bpy = P.y - B.y;
+			cpx = P.x - C.x; cpy = P.y - C.y;
+	 
+			aCROSSbp = ax * bpy - ay * bpx;
+			cCROSSap = cx * apy - cy * apx;
+			bCROSScp = bx * cpy - by * cpx;
+	 
+			return ((aCROSSbp >= 0.0f) && (bCROSScp >= 0.0f) && (cCROSSap >= 0.0f));
+		}
+	}
+	
+		//theory for this comes from:
+		//https://www.topcoder.com/community/competitive-programming/tutorials/geometry-concepts-line-intersection-and-its-applications/
+		//WARNING: Intersections at endpoints are allowed. Shouldn't be an issue in practice, but it's there.
+	public class IntersectionChecker{
+		private bool CheckOneIntersection(Vector3 point1, Vector3 point2, Vector3 point3, Vector3 point4){
+			//checks whether two line segments intersect in the XY plane (setting z=0)
+			//get line equations as Ax + By = C
+			float A1 = point2.y - point1.y;
+			float B1 = point1.x - point2.x;
+			float C1 = A1 * point1.x + B1 * point1.y;
+			float A2 = point4.y - point3.y;
+			float B2 = point3.x - point4.x;
+			float C2 = A2 * point3.x + B2 * point3.y;
+			
+			//solve the system of simultaneous equations for the intersection
+			float delta = A1 * B2 - A2 * B1;
+
+			if (delta == 0) 
+				return false; //lines are parallel
+
+			//find intersection point
+			float x = (B2 * C1 - B1 * C2) / delta;
+			float y = (A1 * C2 - A2 * C1) / delta;
+			
+			//check if it is within segments by elimination of cases
+			if (point1.x <= point2.x){
+				if ((x < point1.x) || (point2.x < x)){
+					return false;
+				}
+			} else{
+				if ((x < point2.x) || (point1.x < x)){
+					return false;
+				}
+			}
+			
+			if (point3.x <= point4.x){
+				if ((x < point3.x) || (point4.x < x)){
+					return false;
+				}
+			} else{
+				if ((x < point4.x) || (point3.x < x)){
+					return false;
+				}
+			}
+			if (point1.y <= point2.y){
+				if ((y < point1.y) || (point2.y < y)){
+					return false;
+				}
+			} else{
+				if ((y < point2.y) || (point1.y < y)){
+					return false;
+				}
+			}
+			
+			if (point3.y <= point4.y){
+				if ((y < point3.y) || (point4.y < y)){
+					return false;
+				}
+			} else{
+				if ((y < point4.y) || (point3.y < y)){
+					return false;
+				}
+			}
+			//no need to check the y's too
+			return true;
+		}
+		
+		public bool CheckIfAddable(Vector3 point1, List<Vector3> points){
+			//returns true if adding the point does not create a self-intersecting polygon
+			//false otherwise
+			Vector3 lastPoint = points[points.Count - 1];
+			Debug.Log("new point: " + point1);
+			Debug.Log("last point: " + lastPoint);
+			for(int i=0;i<points.Count-2;i++){
+				if (CheckOneIntersection(point1, lastPoint, points[i], points[i+1])){
+					return false;
+				}
+			}
+			return true;
+		}
+		
+	}	
+}
--- Assets/Scripts/SpawnObject.cs
+++ Assets/Scripts/SpawnObject.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System;
+using Geometry;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,6 +10,7 @@ public class SpawnObject : MonoBehaviour
     Vector3 currentMousePosition;
 	bool isLoop;
     List<Vector3> points;
+	List<Vector3> screenPoints;
     LineRenderer lrenderer;
     GameObject polygon;
 	Rigidbody2D rb;
@@ -20,11 +22,15 @@ public class SpawnObject : MonoBehaviour
     public float threshold;
 	public Material materializedColor;
 	public Material temporaryColor;
+	public Material cutoutColor;
+	public Material fillColor;
+	IntersectionChecker ic;
     // Start is called before the first frame update
     void Start()
     {
         lastMousePosition = new Vector3(0,0,0); //have a Vector3 ready
         Debug.Log(lastMousePosition);
+		ic = new IntersectionChecker();
 		SetupNewObject();
     }
 
@@ -34,7 +40,7 @@ public class SpawnObject : MonoBehaviour
         if (Input.GetMouseButton(0))
         { //mouse is down
             currentMousePosition = Input.mousePosition;
-			Debug.Log(currentMousePosition);
+			currentMousePosition = camera.ScreenToWorldPoint(new Vector3(currentMousePosition.x, currentMousePosition.y, 10));
 			
             if (((Vector3.Distance(currentMousePosition, lastMousePosition) > threshold) &&
 			(Vector3.Distance(currentMousePosition, lastMousePosition) < maxDrawDistance - distanceDrawn)) ||
@@ -42,17 +48,21 @@ public class SpawnObject : MonoBehaviour
 			//unless it is the first point
             {
 				if (points.Count != 0){ //increase distance drawn if it is not the first point
-					distanceDrawn += Vector3.Distance(currentMousePosition, lastMousePosition); 
+					if (!ic.CheckIfAddable(currentMousePosition, points)){ //separate check because it is long
+						Debug.Log("cannot add, would create self-intersection");
+						return;
+					}
+					distanceDrawn += Vector3.Distance(currentMousePosition, lastMousePosition);
+				} else {
+					
 				}
-                points.Add(camera.ScreenToWorldPoint(new Vector3(currentMousePosition.x, currentMousePosition.y, 10))); //add new point to shape
+					
+                points.Add(currentMousePosition);
                 lrenderer.positionCount++;
 				lrenderer.SetPosition(lrenderer.positionCount-1, points[points.Count-1]); //add the point to temporary line
 				
 				lastMousePosition = currentMousePosition; //update position of last point
-                Debug.Log("Added point");
-            } else {
-				Debug.Log("failed to add point");
-			}
+            }
         }
         else
 		{
@@ -70,17 +80,17 @@ public class SpawnObject : MonoBehaviour
 
     void MaterializeObject()
     {
-		if (Vector3.Distance(points[points.Count-1], points[0]) < 0.25f){
-			isLoop = true;
-		} else{
-			isLoop = false;
+		if (Vector3.Distance(points[points.Count-1], points[0]) > threshold * 10.0f){ //TODO change threshold
+			Destroy(polygon);
+			SetupNewObject();
+			return;
 		}
 		
 		//finalize creation of previous object
 		lrenderer.material = materializedColor;
 		lrenderer.startWidth = 0.05f;
 		lrenderer.endWidth = 0.05f;
-		lrenderer.loop = isLoop;
+		lrenderer.loop = true;
 		
 		//create rigidbody, collider and fill shape
 		rb = polygon.AddComponent(typeof(Rigidbody2D)) as Rigidbody2D;
@@ -89,15 +99,46 @@ public class SpawnObject : MonoBehaviour
 		rb.useAutoMass = true;
 		
 		pCollider = polygon.AddComponent(typeof(PolygonCollider2D)) as PolygonCollider2D;
-		if (isLoop){
-			pCollider.points = ConvertTo2DLoop(points);
-		} else {
-			pCollider.points = ConvertTo2DLine(points);
+		pCollider.points = ConvertTo2DLoop(points);
+		
+		//add mesh to fill object
+		MeshFilter meshFilter = polygon.AddComponent(typeof(MeshFilter)) as MeshFilter;
+		Mesh mesh = new Mesh();
+		MeshRenderer meshRenderer = polygon.AddComponent(typeof(MeshRenderer)) as MeshRenderer;
+		
+		Vector2[] points2d = new Vector2[points.Count + 1];
+		for (int i=0;i<points.Count; i++){
+			points2d[i] = (Vector2) points[i];
+		}
+		points2d[points2d.Length-1] = points[0]; //make it loop back around
+		points2d[points2d.Length-1].x += 0.01f;
+		
+		Vector3[] vertices = new Vector3[points2d.Length];
+		Vector2[] uv = new Vector2[points2d.Length];
+		
+		for(int i=0; i<points2d.Length; i++){
+            Vector2 point = points2d[i];
+            vertices[i] = new Vector3(point.x, point.y, 0);
+            uv[i] = point;
+        }
+		
+		Color[] colors = new Color[points2d.Length];
+		for (int i=0; i<points2d.Length; i++){
+			colors[i] = Color.white;
 		}
 		
-		
+		Triangulator tr = new Triangulator(points2d);
+		int [] triangles = tr.Triangulate();
+		mesh.vertices = vertices;
+		mesh.triangles = triangles;
+		mesh.uv = uv;
+		mesh.colors = colors;
+		meshFilter.mesh = mesh;
+		mesh.RecalculateBounds();
+		meshRenderer.material = fillColor;
 		
 		//create white cutout on background
+		CreateCutout(pCollider.points);
 		
 		Debug.Log("Total distance: " + distanceDrawn);
 		Debug.Log("materialized");
@@ -108,9 +149,11 @@ public class SpawnObject : MonoBehaviour
 	void SetupNewObject()
 	{
 		points = new List<Vector3>();
+		screenPoints = new List<Vector3>();
 		maxDrawDistance = baseMaxDrawDistance;
 		distanceDrawn = 0f;
 		polygon = new GameObject();
+		polygon.name = "Drawn Object";
         lrenderer = polygon.AddComponent(typeof(LineRenderer)) as LineRenderer;
         lrenderer.startWidth = 0.1f;
         lrenderer.endWidth = 0.1f;
@@ -120,26 +163,54 @@ public class SpawnObject : MonoBehaviour
 		lrenderer.material = temporaryColor;
 	}
 	
+	void CreateCutout(Vector2[] basePoints){
+		Vector2[] points = new Vector2[basePoints.Length + 1];
+		for (int i=0;i<basePoints.Length; i++){
+			points[i] = basePoints[i];
+		}
+		points[points.Length-1] = basePoints[0]; //make it loop back around
+		points[points.Length-1].x += 0.01f;
+		GameObject cutout = new GameObject();
+		cutout.name = "Cutout";
+		PolygonCollider2D cutoutCollider = cutout.AddComponent(typeof(PolygonCollider2D)) as PolygonCollider2D;
+		cutout.layer = 8; //layer on which raycast will be used for cutouts
+		MeshFilter meshFilter = cutout.AddComponent(typeof(MeshFilter)) as MeshFilter;
+		Mesh mesh = new Mesh();
+		MeshRenderer meshRenderer = cutout.AddComponent(typeof(MeshRenderer)) as MeshRenderer;
+		
+		
+		Vector3[] vertices = new Vector3[points.Length];
+		Vector2[] uv = new Vector2[points.Length];
+		
+		for(int i=0; i<points.Length; i++){
+            Vector2 point = points[i];
+            vertices[i] = new Vector3(point.x, point.y, 0);
+            uv[i] = point;
+        }
+		
+		Color[] colors = new Color[points.Length];
+		for (int i=0; i<points.Length; i++){
+			colors[i] = Color.white;
+		}
+		
+		Triangulator tr = new Triangulator(points);
+		int [] triangles = tr.Triangulate();
+		mesh.vertices = vertices;
+		mesh.triangles = triangles;
+		mesh.uv = uv;
+		mesh.colors = colors;
+		meshFilter.mesh = mesh;
+		mesh.RecalculateBounds();
+		meshRenderer.material = cutoutColor;
+	}
+	
 	Vector2[] ConvertTo2DLoop(List<Vector3> points3d)
 	{
 		Vector2[] points2d = new Vector2[points3d.Count+1];
 		for (int i = 0; i < points3d.Count; i++){
 			points2d[i] = (Vector2) points3d[i];
 		}
-		points2d[points2d.Length-1] = (Vector3) points3d[0]; //repeat the first point at the end to loop around
-		return points2d;
-	}
-	
-	Vector2[] ConvertTo2DLine(List<Vector3> points3d)
-	{
-		Vector2[] points2d = new Vector2[points3d.Count*2-1];
-		for (int i = 0; i < points3d.Count; i++){
-			points2d[i] = (Vector2) points3d[i];
-		}
-		for (int i = 0; i < points3d.Count; i++){
-			points2d[points2d.Length-i-1] = (Vector2) points3d[i];
-			points2d[points2d.Length-i-1].x += 0.07f; //give the line some width
-		}
+		points2d[points2d.Length-1] = (Vector2) points3d[0]; //repeat the first point at the end to loop around
 		return points2d;
 	}
 }
