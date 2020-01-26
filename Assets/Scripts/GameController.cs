b272246139d83c87a3731548edcf3ad120aaeebd
Andrei486 <55627866+Andrei486@users.noreply.github.com>
2020-01-26 16:57:46 -0500

    Bumpers...
    Added bouncy bumpers that make things gain speed on collision.
    Bad idea.
    More importantly, added an end condition for the game; still need some sort of UI text to appear on screen.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,6 +7,7 @@ public class GameController : MonoBehaviour
 	public GameObject player;
 	public Vector3[] spawnPositions;
 	GameObject[] players;
+	bool ended;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,10 +15,12 @@ public class GameController : MonoBehaviour
 		players = new GameObject[2];
 		
         players[0] = Instantiate(player, this.gameObject.transform);
+		players[0].name = "Player 1";
 		players[0].transform.position = spawnPositions[0];
 		players[0].GetComponent<PolygonCollider2D>().enabled = true;
 		
 		players[1] = Instantiate(player, this.gameObject.transform);
+		players[1].name = "Player 2";
 		players[1].transform.position = spawnPositions[1];
 		players[1].GetComponent<PolygonCollider2D>().enabled = true;
 		//flip the second player
@@ -28,6 +31,30 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+		if (!ended){
+			if (players[0] == null){
+				Debug.Log("Player 2 wins!");
+				ended = true;
+				StartCoroutine(EndGame());
+			}
+			if (players[1] == null){
+				Debug.Log("Player 1 wins!");
+				ended = true;
+				StartCoroutine(EndGame());
+			}
+		}
+		else {
+			if (Input.GetKeyDown("space")){
+				Application.Quit();
+			}
+		}
     }
+	
+	private IEnumerator EndGame(){
+		
+		//show the win text
+		Time.timeScale = 0.1f;
+		yield return new WaitForSeconds(0.5f);
+		Application.Quit();
+	}
 }
