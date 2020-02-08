7578962be87d17a097c63fa497d3920dc8434c6e
Andrei486 <55627866+Andrei486@users.noreply.github.com>
2020-02-07 22:02:57 -0500

    Kliff model update
    Player prefabs now use the Kliff 3D model. It has a custom-made polygon collider 2D, as well as basic idle animations from the game.
    Issues to fix:
    -get texture
    -make it be lit
    -react on getting hit with animation, and on death

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlayerController : MonoBehaviour
 {
 	public Health healthScript;
+	Animator animator;
+	System.Random rng;
     // Start is called before the first frame update
     void Start()
     {
         healthScript = this.gameObject.GetComponent<Health>();
+		animator = this.gameObject.transform.Find("Kliff_Model").GetComponent<Animator>();
+		rng = new System.Random();
+		StartCoroutine(RandomIdle());
     }
 
     // Update is called once per frame
@@ -23,12 +29,30 @@ public class PlayerController : MonoBehaviour
 		if (collision.collider.tag != "Drawn"){
 			return;
 		}
-		float momentum = collision.rigidbody.velocity.magnitude * collision.rigidbody.mass;
+		float momentum = collision.relativeVelocity.magnitude * collision.rigidbody.mass;
 		healthScript.TakeDamage(momentum);
 	}
 	
 	public void Die(){
 		//activate ragdoll
 		Destroy(this.gameObject);
+	}
+	
+	IEnumerator RandomIdle(){
+		while (true) {
+			if (animator.GetCurrentAnimatorStateInfo(0).IsName("Basic Idle")){
+				double randomness = rng.NextDouble();
+				Debug.Log(randomness);
+				if (randomness < 0.1f){
+					animator.ResetTrigger("Idle2");
+					animator.SetTrigger("Idle1");
+				} else if (randomness < 0.3f){
+					animator.ResetTrigger("Idle1");
+					animator.SetTrigger("Idle2");
+				}
+			}
+			yield return new WaitForSeconds(0.5f);
+		}
+		
 	}
 }
